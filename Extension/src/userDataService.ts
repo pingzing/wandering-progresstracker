c481718ce5b1b6967dc075ec67197d5fb4344e6f
pingzing <mcalistern@gmail.com>
2023-11-27 23:23:40 +0200

    Storage saving and loading works

--- Extension/src/userDataService.ts
+++ Extension/src/userDataService.ts
@@ -1,28 +1,31 @@
 import browser from 'webextension-polyfill';
 import type { ChapterInfo, ChapterUserInfo, StoryUrl, UserData } from './models';
+import { gzipSync, decompressSync, strFromU8, strToU8 } from 'fflate';
 
 class UserDataService {
-    private static readonly savedChaptersCountKey = `savedChaptersCount`;
-    private static readonly savedChaptersKeyPrefix = 'savedChapters-';
+    private static readonly savedChapterChunkCountKey = `savedChaptersCount`;
+    private static readonly savedChaptersKeyPrefix = 'savedChapters';
+    private static readonly textEncoder = new TextEncoder();
+    private static readonly textDecoder = new TextDecoder();
 
     /**
      * Get saved user data, including chapter progress.
      */
     public async getUserData(): Promise<UserData> {
-        const data = (await browser.storage.sync.get(UserDataService.savedChaptersKeyPrefix))[
-            UserDataService.savedChaptersKeyPrefix
-        ];
+        const savedChapterCount = (await browser.storage.sync.get(UserDataService.savedChapterChunkCountKey))[
+            UserDataService.savedChapterChunkCountKey
+        ] as number | undefined;
 
-        if (!data || Object.keys(data).length === 0) {
-            console.log(`No user data found, creating empty data.`)
+        if (!savedChapterCount || savedChapterCount === 0) {
             const emptyUserData: UserData = {
                 savedChapters: new Map<StoryUrl, ChapterUserInfo>()
             };
             return emptyUserData;
         }
 
-        const deserializedData = this.deserialize(data);
-        return { savedChapters: deserializedData };
+        return {
+            savedChapters: await this.getChaptersFromStorage()
+        };
     }
 
     /**
@@ -40,9 +43,7 @@ class UserDataService {
             });
         }
 
-        await browser.storage.sync.set({
-            [UserDataService.savedChaptersKeyPrefix]: this.serialize(savedChapters)
-        });
+        await this.saveChaptersToStorage(savedChapters);
     }
 
     /**
@@ -55,13 +56,27 @@ class UserDataService {
             savedChapters.set(url, chapterInfo);
         }
 
-        await browser.storage.sync.set({
-            [UserDataService.savedChaptersKeyPrefix]: this.serialize(savedChapters)
-        });
+        await this.saveChaptersToStorage(savedChapters);
     }
 
     private async getChaptersFromStorage(): Promise<Map<StoryUrl, ChapterUserInfo>> {
-        const chapterCount = (await browser.storage.sync.get(UserDataService.savedChaptersCountKey))[UserDataService.savedChaptersCountKey] as number;
+        const chapterCount = (await browser.storage.sync.get(UserDataService.savedChapterChunkCountKey))[UserDataService.savedChapterChunkCountKey] as number | undefined;
+        if (!chapterCount || chapterCount === 0) {
+            return new Map<StoryUrl, ChapterUserInfo>();
+        }
+
+        const retrievedChapters = new Map<StoryUrl, ChapterUserInfo>();
+        const keys = [...Array(chapterCount).keys()].map(x => `${UserDataService.savedChaptersKeyPrefix}-${x}`);
+        const chunks = (await browser.storage.sync.get(keys));
+        for (const key of keys) {
+            const serializedChunk = chunks[key];
+            const deserializedChunk = this.deserialize(serializedChunk);
+            for (const [url, chapterUserInfo] of deserializedChunk) {
+                retrievedChapters.set(url, chapterUserInfo);
+            }
+        }
+
+        return retrievedChapters;
     }
 
     /**
@@ -71,21 +86,23 @@ class UserDataService {
      * @param allChapters **ALL** known chapters.
      */
     private async saveChaptersToStorage(allChapters: Map<StoryUrl, ChapterUserInfo>): Promise<void> {
-        const chunkSize = 50;
+
+        // Break chapters into chunks, because there are too many to fit into a single storage entry. 
+        // 10 is arbitrarily "probably small enough".
+        const chunkSize = 10;
         const iterableChapters = Array.from(allChapters.entries());
-        const chaptersToSave: { key: string, value: { [k: StoryUrl]: ChapterUserInfo } }[] = [];
+        const chaptersToSave: { key: string, value: string }[] = [];
         let savedChapterCount = 0;
         for (let i = 0; i < iterableChapters.length; i += chunkSize) {
             const chunk = iterableChapters.slice(i, i + chunkSize);
-            const objectifiedChapterChunk = Object.fromEntries(chunk);
 
             chaptersToSave.push({
                 key: `${UserDataService.savedChaptersKeyPrefix}-${savedChapterCount}`,
-                value: objectifiedChapterChunk
+                value: this.serialize(chunk),
             });
 
             savedChapterCount += 1;
-        }
+        }        
 
         for (const chapterChunk of chaptersToSave) {
             await browser.storage.sync.set({
@@ -94,18 +111,38 @@ class UserDataService {
         }
 
         await browser.storage.sync.set({
-            [UserDataService.savedChaptersCountKey]: savedChapterCount
+            [UserDataService.savedChapterChunkCountKey]: savedChapterCount
         });
     }
 
-    private serialize(map: Map<StoryUrl, ChapterUserInfo>): string {
-        const object = Object.fromEntries(map.entries());
-        return JSON.stringify(object);
+    private serialize(chapterChunk: [StoryUrl, ChapterUserInfo][]): string {
+        // When serializing, strip out the domain name to save space
+        for (const entry of chapterChunk) {
+            entry[0] = entry[0].replace(`https://wanderinginn.com`, ``);
+        }
+
+        // Also, compress to save even more space
+        const string = JSON.stringify(chapterChunk);
+        const byteString = UserDataService.textEncoder.encode(string);
+        const compressedBytes = gzipSync(byteString);
+
+        // And turn the compressed bytes BACK into a string, because we can't save Uint8Arrays directly.
+        const stringifiedBytes = strFromU8(compressedBytes, true);
+        return stringifiedBytes;
     }
 
-    private deserialize(serializedMap: string): Map<StoryUrl, ChapterUserInfo> {
-        const object = JSON.parse(serializedMap);
-        return new Map(Object.entries(object));
+    private deserialize(stringifiedCompressedBytes: string): [StoryUrl, ChapterUserInfo][] {
+        if (!stringifiedCompressedBytes.length || stringifiedCompressedBytes.length === 0) {
+            throw new Error(`Invalid string length when calling deserialize. Received: ${stringifiedCompressedBytes}`);
+        }
+        const compressedBytes = strToU8(stringifiedCompressedBytes, true);
+        const decompressedString = decompressSync(compressedBytes);
+        const decodedString = UserDataService.textDecoder.decode(decompressedString);
+        const chapterChunks: [StoryUrl, ChapterUserInfo][] = JSON.parse(decodedString);
+        for (const chunk of chapterChunks) {
+            chunk[0] = `https://wanderinginn.com${chunk[0]}`;
+        }
+        return chapterChunks;
     }
 }
 
