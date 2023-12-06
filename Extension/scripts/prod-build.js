ee4796f7f8d796b51062bb32d1cf81d7deec5a5a
pingzing <mcalistern@gmail.com>
2023-12-06 16:27:13 +0200

    Add check to build script for dist folder before trying to delete

--- Extension/scripts/prod-build.js
+++ Extension/scripts/prod-build.js
@@ -5,7 +5,9 @@ const exec = util.promisify(require('child_process').exec);
 main();
 
 async function main() {
-  await fs.rm('./dist', { recursive: true });
+  if (fs.existsSync('./dist')) {
+    await fs.rm('./dist', { recursive: true });
+  }
   await fs.mkdir('./dist');
   await exec('npm run build -- --prod');
   console.log('Done');
