3de53efd1360cd2cce15bd605e39c20dc9dd326b
pingzing <mcalistern@gmail.com>
2023-12-06 16:30:04 +0200

    Import correct package for fs.existsSync()

--- Extension/scripts/prod-build.js
+++ Extension/scripts/prod-build.js
@@ -1,11 +1,12 @@
 const fs = require('fs/promises');
+const fsExtra = require('fs-extra');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 main();
 
 async function main() {
-  if (fs.existsSync('./dist')) {
+  if (fsExtra.existsSync('./dist')) {
     await fs.rm('./dist', { recursive: true });
   }
   await fs.mkdir('./dist');
